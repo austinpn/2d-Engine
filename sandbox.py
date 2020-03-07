34c3ef1ce1710819ca915586879bd294f7be06c5
Austin <novellyaus@gmail.com>
2020-03-07 14:37:47 -0600

    still broken idk

--- sandbox.py
+++ sandbox.py
@@ -3,5 +3,5 @@ testvel = [0 , 0]
 forces = [[0 , 0]]
 
 for x in range(100):
-    Physics.applyForce(testvel , testvel , forces , 0)
+    Physics.applyForce(testvel , testvel , forces)
 print(testvel)
